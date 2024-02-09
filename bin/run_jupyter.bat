052841317e9e7683c2e603b75d1d079688dd33ac
Todd Kushner <kushnertodd@gmail.com>
2024-02-09 13:04:18 -0700

    readme

---
+++ bin/run_jupyter.bat
@@ -0,0 +1,2 @@
+jupyter notebook --notebook-dir=C:\Users\kushn\Dropbox\software\projects\teaching\kotlin-programming\notebooks 2> nul  >nul
+
---
+++ bin/run_jupyter.sh
@@ -0,0 +1,1 @@
+jupyter-notebook --no-browser --port=8000 --notebook-dir=~/kotlin-programming/notebooks 2>/dev/null >/dev/null
