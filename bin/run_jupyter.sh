716b00deb23b2841373262b881498aca3db3c29c
Todd Kushner <kushnertodd@gmail.com>
2024-02-09 13:17:52 -0700

    readme

--- bin/run_jupyter.sh
+++ bin/run_jupyter.sh
@@ -1,1 +1,1 @@
-jupyter-notebook --no-browser --port=8000 --notebook-dir=~/kotlin-programming/notebooks 2>/dev/null >/dev/null
+jupyter-notebook --no-browser --port=8000 --notebook-dir=~/kotlin-programming/notebooks 2>/dev/null >/dev/null &
